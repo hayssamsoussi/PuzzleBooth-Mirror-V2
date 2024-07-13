8644b1f9db06a3195f743e615a39d7e36d65252f
hayssam <hayssamsoussi@gmail.com>
2024-07-13 13:56:36 +0300

    bump up to 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
         minSdk = 26
         targetSdk = 34
         versionCode = 5
-        versionName = "2.0"
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -47,15 +47,15 @@ android {
         create("remote") {
             dimension = "version"
             applicationId = "com.puzzlebooth.remote"
-            versionCode = 3
-            versionName = "2.0"
+            versionCode = 4
+            versionName = "2.1"
         }
 
         create("server") {
             dimension = "version"
             applicationId = "com.puzzlebooth.server"
-            versionCode = 3
-            versionName = "2.0"
+            versionCode = 4
+            versionName = "2.1"
         }
     }
 }
